586baa6d644f95ab144ae34d7aa0b396d471742a
SrikarAnanthula <ananthulasrikar@gmail.com>
2023-03-17 13:09:26 -0700

    Update gpg.sh

--- playbooks/macbook/gpg.sh
+++ playbooks/macbook/gpg.sh
@@ -7,3 +7,4 @@ gpg --import $GITHUB_GPG_KEY_PATH
 # trust ~> 5 for ultimate
 # save
 gpg --edit-key 783594+ananthulasrikar@users.noreply.github.com
+git config --global user.signingkey ACC94F89CF83034C
